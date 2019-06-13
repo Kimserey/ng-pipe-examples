fb3e930c575883b3b5355c3cac6fa49217656032
Kimserey <kimserey.lam@gmail.com>
2019-06-13 11:00:16 +0100

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   amount: number = 123;
+
   today: Date = new Date();
+
   decimalValue: number = 123.432432;
+  
   i18nValue: string = 'female';
+
   i18nValueMap: any = {
     'male': 'He',
     'female': 'She'
   };
+
   i18nPluralValue: number = 3;
+
   // http://userguide.icu-project.org/formatparse/messages
   i18nPluralValueMap: any = {
     '=0': 'Nothing',
     '=1': 'We have one',
     'other': 'We have a few'
   };
+
   objValue: any = {
     one: 'Hello',
     two: 'Bye'
   };
+
   phrase: string = 'Hello this is a test';
+
   percentage: number = 0.1;
 }
